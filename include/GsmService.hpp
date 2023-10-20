e86ed26af2a4206f70e5ed3d67a797a49dd35341
xen <selegakamil@gmail.com>
2023-10-21 01:16:44 +0200

    Check module connection, network registration, connect to GPRS

--- include/GsmService.hpp
+++ include/GsmService.hpp
@@ -6,12 +6,27 @@ using uart::UartController;
 
 namespace gsm {
 
+using AtCommand = std::string;
+
+struct Response {
+    bool valid;
+    std::string response;
+};
+
 class GsmController {
 public:
     GsmController(UartController&& uart);
 
     void eventLoop();
-    bool checkConnection();
+
+    void sendAtCommand(const AtCommand& command);
+    Response getResponse();
+
+    bool moduleConnected();
+    bool networkConnected();
+    bool gprsConnected();
+
+    void connectGprs();
 
 private:
     UartController& uart;
--- src/GsmService.cpp
+++ src/GsmService.cpp
@@ -7,23 +7,104 @@ namespace gsm {
 static constexpr char logTag[] = "GSM";
 
 GsmController::GsmController(UartController&& uart) : uart(uart) {
-    eventLoop();
 }
 
 void GsmController::eventLoop() {
     while (true) {
-        ESP_LOGI(logTag, "%s: Checking connection = %d", __func__, checkConnection());
+        if (!moduleConnected()) {
+            ESP_LOGW(logTag, "%s: GSM module is not connected", __func__);
+            continue;
+        }
+        if (!networkConnected()) {
+            ESP_LOGW(logTag, "%s: No connection to the network", __func__);
+            continue;
+        }
+        if (!gprsConnected()) {
+            ESP_LOGI(logTag, "%s: Connecting to GPRS", __func__);
+            connectGprs();
+        }
     }
 }
 
-bool GsmController::checkConnection() {
-    uart.write("AT\r\n");
-    const auto response = uart.read();
-    if (response.find("OK") != std::string::npos) {
-        ESP_LOGD(logTag, "%s: GSM module is connected", __func__);
-        return true;
-    }
-    return false;
+void GsmController::sendAtCommand(const AtCommand& command) {
+    uart.write(command + "\r");
+}
+
+Response GsmController::getResponse() {
+    const auto responseString = uart.read();
+    return Response{
+        .valid = responseString.contains("OK"),
+        .response = responseString,
+    };
+}
+
+bool GsmController::moduleConnected() {
+    sendAtCommand("AT");
+    return getResponse().valid;
+}
+
+bool GsmController::networkConnected() {
+    sendAtCommand("AT+CREG?");
+    return getResponse().valid;
+}
+
+bool GsmController::gprsConnected() {
+    sendAtCommand("AT+CGREG?");
+    return getResponse().valid;
+}
+
+void GsmController::connectGprs() {
+    const std::string apn = "internet";
+    const std::string user = "";
+    const std::string password = "";
+
+    sendAtCommand("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");
+    getResponse();
+
+    sendAtCommand("AT+SAPBR=3,1,\"APN\",\"" + apn + "\"");
+    getResponse();
+
+    sendAtCommand("AT+SAPBR=3,1,\"USER\",\"" + user + "\"");
+    getResponse();
+
+    sendAtCommand("AT+SAPBR=3,1,\"PWD\",\"" + password + "\"");
+    getResponse();
+
+    sendAtCommand("AT+CGDCONT=1,\"IP\",\"" + apn + "\"");
+    getResponse();
+
+    sendAtCommand("AT+CGACT=1,1");
+    getResponse();
+
+    sendAtCommand("AT+SAPBR=1,1");
+    getResponse();
+
+    sendAtCommand("AT+SAPBR=2,1");
+    getResponse();
+
+    sendAtCommand("AT+CGATT=1");
+    getResponse();
+
+    sendAtCommand("AT+CIPMUX=1");
+    getResponse();
+
+    sendAtCommand("AT+CIPQSEND=1");
+    getResponse();
+
+    sendAtCommand("AT+CIPRXGET=1");
+    getResponse();
+
+    sendAtCommand("AT+CSTT=\"" + apn + "\",\"" + user + "\",\"" + password + "\"");
+    getResponse();
+
+    sendAtCommand("AT+CIICR");
+    getResponse();
+
+    sendAtCommand("AT+CIFSR;E0");
+    getResponse();
+
+    sendAtCommand("AT+CDNSCFG=\"8.8.8.8\",\"8.8.4.4\"");
+    getResponse();
 }
 
 }  // namespace gsm
